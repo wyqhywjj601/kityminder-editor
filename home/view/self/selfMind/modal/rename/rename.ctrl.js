572a832c6238ae519376a323960886b4d4aac047
魏俊杰 <weijunjie@wtoip.com>
2018-10-16 14:33:40 +0800

    feat: 重命名弹窗添加

---
+++ home/view/self/selfMind/modal/rename/rename.ctrl.js
@@ -0,0 +1,15 @@
+angular.module('mindManage')
+    .controller('rename.ctrl', function ($scope, $modalInstance,modalDatas) {
+        var $ctrl = this;
+
+        // $ctrl.insta
+        $scope.renameValue = '';
+        $scope.modalDatas = modalDatas;
+        $scope.ok = function () {
+            var renameValue = $scope.renameValue;
+           
+            $modalInstance.close(
+                $scope.modalDatas //在模态框View中修改的值传递回去，view中可以直接添加属性
+            );
+        };
+    });
--- home/view/self/selfMind/selfMind.ctrl.js
+++ home/view/self/selfMind/selfMind.ctrl.js
@@ -1,4 +1,32 @@
  angular.module("mindManage")
-     .controller('selfMindCtrl', function ($scope) {
-         console.log(123)
-     });
+     .controller('selfMindCtrl', ['$modal', '$scope', function ($modal, $scope) {
+
+         $scope.fileRename = function () {
+             // 重命名弹窗
+
+             var modalDatas = {
+                renameValue:'222'
+             };
+
+             var renameModal = $modal.open({
+                 animation: true,
+                 templateUrl: 'home/view/self/selfMind/modal/rename/rename.tpl.html',
+                 controller: 'rename.ctrl',
+                 size: 'md',
+                 resolve: {
+                    modalDatas: function () {
+                         return modalDatas;
+                     }
+                 }
+             });
+
+             renameModal.result.then(function (result) {
+                 console.log(result);
+                 // minder.execCommand('image', result.url, result.title || '');
+             },function(){
+                console.log('cancel')
+             });
+
+         }
+
+     }]);

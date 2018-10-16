2ab3a7104b9e61316ac7402b1449e39704b3b741
魏俊杰 <weijunjie@wtoip.com>
2018-10-16 17:05:58 +0800

    feat: 批量选择

--- home/view/self/selfMind/modal/rename/rename.ctrl.js
+++ home/view/self/selfMind/modal/rename/rename.ctrl.js
@@ -1,5 +1,5 @@
 angular.module('mindManage')
-    .controller('rename.ctrl', function ($scope, $modalInstance,modalDatas) {
+    .controller('rename.ctrl', function ($scope, $modalInstance, modalDatas) {
         var $ctrl = this;
 
         // $ctrl.insta
@@ -7,9 +7,15 @@ angular.module('mindManage')
         $scope.modalDatas = modalDatas;
         $scope.ok = function () {
             var renameValue = $scope.renameValue;
-           
+
+            // 点击确认事件
             $modalInstance.close(
                 $scope.modalDatas //在模态框View中修改的值传递回去，view中可以直接添加属性
             );
         };
+
+            // 点击取消事件
+        $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+        }
     });
